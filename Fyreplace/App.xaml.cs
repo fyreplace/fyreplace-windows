71908ae97a03f6e10e3e68e22040055971f15de1
Laurent Tréguier <laurent.treguier@fyreplace.app>
2024-06-20 15:17:20 +0200

    Add Sentry

--- Fyreplace/App.xaml.cs
+++ Fyreplace/App.xaml.cs
@@ -1,4 +1,10 @@
 ﻿using Microsoft.UI.Xaml;
+using Sentry;
+using Sentry.Protocol;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Security;
 
 namespace Fyreplace
 {
@@ -6,12 +12,55 @@ namespace Fyreplace
     {
         private Window? window;
 
-        public App() => InitializeComponent();
+        public App()
+        {
+            var dsn = GetCustomAttribute("Sentry.Dsn");
+
+            if (dsn != "")
+            {
+                SentrySdk.Init(options =>
+                {
+                    options.Dsn = dsn;
+                    options.Environment = GetCustomAttribute("Sentry.Environment");
+                    options.AutoSessionTracking = true;
+                    options.IsGlobalModeEnabled = true;
+                    options.CaptureFailedRequests = true;
+                    options.DisableWinUiUnhandledExceptionIntegration();
+                });
+
+                UnhandledException += OnUnhandledException;
+            }
+
+            InitializeComponent();
+        }
 
         protected override void OnLaunched(LaunchActivatedEventArgs args)
         {
             window = new MainWindow();
             window.Activate();
         }
+
+        [SecurityCritical]
+        private void OnUnhandledException(object sender, Microsoft.UI.Xaml.UnhandledExceptionEventArgs e)
+        {
+            var exception = e.Exception;
+
+            if (exception == null)
+            {
+                return;
+            }
+
+            exception.Data[Mechanism.HandledKey] = false;
+            exception.Data[Mechanism.MechanismKey] = "Application.UnhandledException";
+            SentrySdk.CaptureException(exception);
+            SentrySdk.FlushAsync(TimeSpan.FromSeconds(3)).GetAwaiter().GetResult();
+        }
+
+        private static string GetCustomAttribute(string key) => Assembly.GetExecutingAssembly()
+                .GetCustomAttributes(false)
+                .OfType<AssemblyMetadataAttribute>()
+                .Where(a => a.Key == key)
+                .Select(a => a.Value)
+                .FirstOrDefault() ?? "";
     }
 }
