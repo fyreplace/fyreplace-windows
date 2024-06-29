e62d44d625320e52fdf9c453537e0e8265d4969f
Laurent Tréguier <laurent.treguier@fyreplace.app>
2024-06-29 15:39:37 +0200

    Build MSI installer

--- prepare.bat
+++ prepare.bat
@@ -1,2 +1,3 @@
 copy Fyreplace\app.template.manifest Fyreplace\app.manifest
 copy Fyreplace\Package.template.appxmanifest Fyreplace\Package.appxmanifest
+copy Fyreplace.Installer\Package.template.xml Fyreplace.Installer\Package.wxs
