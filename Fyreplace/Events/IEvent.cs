af2c7d08e345aa3726f371333adebb54a1c37334
Laurent Tréguier <laurent.treguier@fyreplace.app>
2024-09-05 13:06:21 +0200

    Fix unknown errors

--- Fyreplace/Events/IEvent.cs
+++ Fyreplace/Events/IEvent.cs
@@ -2,13 +2,13 @@
 {
     public interface IEvent { }
 
-    public readonly record struct FailureEvent(string Key = "Error_Unknown") : IEvent
+    public record class FailureEvent(string Key = "Error_Unknown") : IEvent
     {
         public string Title => $"{Key}/{nameof(Title)}";
         public string Message => $"{Key}/{nameof(Message)}";
     }
 
-    public readonly record struct PreferenceChangedEvent(string Name) : IEvent { }
+    public record class PreferenceChangedEvent(string Name) : IEvent { }
 
-    public readonly record struct SecretChangedEvent(string Name) : IEvent { }
+    public record class SecretChangedEvent(string Name) : IEvent { }
 }
